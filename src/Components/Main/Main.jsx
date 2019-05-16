3c77a9df5f218281c158fc9b970f9f6a07c921ac
lyzengar <darthmaul85@hotmail.com>
2019-05-16 09:43:28 -0700

    update styles

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Main.css'
+import TopScreen from '../TopScreen/TopScreen'
+import BottomScreen from '../BottomScreen/BottomScreen'
 
 const Main = (props) => {
     return (
         <div className="main">
             <img src="pokedex.png"></img>
+            <TopScreen />
+            <BottomScreen />
         </div>
     )
 }

7e86d31a1728dd5aca8cde2428e7cb7fb64808be
lyzengar <darthmaul85@hotmail.com>
2019-05-21 14:43:59 -0700

    update component

---
+++ src/Components/SelectPok/SelectPok.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SelectPok.css';
+
+const SelectPok = (props) => {
+    return(
+        <div className="select-pok">
+        
+        </div>
+    )
+}
+
+export default SelectPok;

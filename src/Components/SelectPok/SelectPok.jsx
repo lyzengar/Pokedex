591d6e3bac5f3d8ebf1f5e26fc99990902c42b28
lyzengar <darthmaul85@hotmail.com>
2019-09-17 14:55:50 -0700

    add pokemon info

--- src/Components/SelectPok/SelectPok.jsx
+++ src/Components/SelectPok/SelectPok.jsx
@@ -83,8 +83,8 @@ const SelectPok = (props) => {
                 <option value="golem">076 - Golem</option>
                 <option value="ponyta">077 - Ponyta</option>
                 <option value="rapidash">078 - Rapidash</option>
-                <option value="slowpoke">077 - Slowpoke</option>
-                <option value="slowbro">078 - Slowbro</option>
+                <option value="slowpoke">079 - Slowpoke</option>
+                <option value="slowbro">080 - Slowbro</option>
             </select>
         </div>
     )
